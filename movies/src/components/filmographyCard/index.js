2c24ee1db7bd7ba6abb452c520a3fdd30f65e82d
JayLangfordGitHub <jaylangfordprf@gmail.com>
2023-11-23 08:59:37 +0000

    made moviecard and filographycard match up to look the same, also made them look more apealing

--- movies/src/components/filmographyCard/index.js
+++ movies/src/components/filmographyCard/index.js
@@ -8,42 +8,49 @@ import { Link } from "react-router-dom";
 import img from '../../images/film-poster-placeholder.png';
 import { CardActions } from "@mui/material";
 import Button from '@mui/material/Button';
+import StarRateIcon from '@mui/icons-material/StarRate'; // Import the star icon
 
 export default function FilmographyCard({ movie }) {
-  return (
-    <Card sx={{ width: '100%', maxWidth: 400 }}> {/* Updated maxWidth */}
-      <CardMedia
-        sx={{ height: 500 }} // You might want to adjust this as well
-        image={
-          movie.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-            : img
-        }
-      />
-      <CardContent>
-        <Typography variant="h5" component="p">
-          {movie.title}
-        </Typography>
-        <Grid container spacing={1}>
-          <Grid item xs={12}> 
-            <Typography variant="body2" color="text.secondary">
-              Release Date: {movie.release_date}
+
+    const formattedRating = (rating) => {
+        return rating ? rating.toFixed(1) : 'N/A';
+      };
+
+      return (
+        <Card sx={{ width: '100%', maxWidth: 400 }}>
+          <CardMedia
+            sx={{ height: 500 }}
+            image={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : img
+            }
+          />
+          <CardContent>
+            <Typography variant="h5" component="p">
+              {movie.title}
             </Typography>
-          </Grid>
-          <Grid item xs={12}> 
-            <Typography variant="body2" color="text.secondary">
-              Rating: {movie.vote_average}
-            </Typography>
-          </Grid>
-        </Grid>
-      </CardContent>
-      <CardActions>
-        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
-          <Button variant="outlined" size="medium" color="primary">
-            More Info ...
-          </Button>
-        </Link>
-      </CardActions>
-    </Card>
-  );
-}
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <Typography variant="body2" color="text.secondary" component="div">
+                  <StarRateIcon sx={{ verticalAlign: 'bottom' }} />
+                  {` ${formattedRating(movie.vote_average)}`} {/* Format the rating */}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}> 
+                <Typography variant="body2" color="text.secondary" component="div">
+                  Release Date: {movie.release_date}
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+          <CardActions>
+            <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
+              <Button variant="outlined" size="medium" color="primary">
+                More Info ...
+              </Button>
+            </Link>
+          </CardActions>
+        </Card>
+      );
+    }
--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,97 +1,57 @@
-import React, { useContext  } from "react";
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CardHeader from "@mui/material/CardHeader";
-import Button from "@mui/material/Button";
+import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
-import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
-import img from '../../images/film-poster-placeholder.png'
-import Avatar from '@mui/material/Avatar';
+import img from '../../images/film-poster-placeholder.png';
 import { Link } from "react-router-dom";
-import { MoviesContext } from "../../contexts/moviesContext";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Button from "@mui/material/Button";
+import { CardActions } from "@mui/material";
 
-export default function MovieCard({ movie, action }) {
-
+export default function MovieCard({ movie }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
-  const { mustWatch, addToPlaylist } = useContext(MoviesContext);
 
-  if (favorites.find((id) => id === movie.id)) {
-    movie.favorite = true;
-  } else {
-    movie.favorite = false
-  }
-
-  if (mustWatch.find((id) => id === movie.id)) {
-    movie.mustWatch = true;
-  } else {
-    movie.mustWatch = false
-  }
+  const isFavorite = favorites.some(id => id === movie.id);
 
   const handleAddToFavorite = (e) => {
-    e.preventDefault();
+    e.stopPropagation();
     addToFavorites(movie);
-  };
-
-  const handleAddToPlaylist = (e) => {
-    e.preventDefault();
-    addToPlaylist(movie);
   };
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          movie.favorite ? (
-            <Avatar sx={{ backgroundColor: 'red' }}>
-              <FavoriteIcon />
-            </Avatar>
-          ) :
-          movie.mustWatch ? (
-            <Avatar sx={{ backgroundColor: 'blue' }}>
-                <PlaylistAddIcon />
-            </Avatar>
-          ) : null
-        }
-        title={
-          <Typography variant="h5" component="p">
-            {movie.title}{" "}
-          </Typography>
-        }
-      />
       <CardMedia
         sx={{ height: 500 }}
-        image={
-          movie.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-            : img
-        }
+        image={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : img}
       />
       <CardContent>
-        <Grid container>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="p">
-              <CalendarIcon fontSize="small" />
-              {movie.release_date}
+        <Typography gutterBottom variant="h5" component="div">
+          {movie.title}
+        </Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="body2" color="text.secondary" component="div">
+              <StarRateIcon sx={{ verticalAlign: 'bottom' }} />
+              {` ${movie.vote_average}`}
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="p">
-              <StarRateIcon fontSize="small" />
-              {"  "} {movie.vote_average}{" "}
+          <Grid item xs={12}>
+            <Typography variant="body2" color="text.secondary" component="div">
+              Release Date: {movie.release_date}
             </Typography>
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions disableSpacing>
-        {action(movie)}
-        <Link to={`/movies/${movie.id}`}>
+      <CardActions sx={{ justifyContent: 'flex-start', padding: '8px' }}>
+        <IconButton onClick={handleAddToFavorite} size="large">
+          <FavoriteIcon color={isFavorite ? "error" : "primary"} />
+        </IconButton>
+        <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none' }}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
